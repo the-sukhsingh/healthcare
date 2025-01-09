e629543d82067481962cbebbed8e1be2eb08db8b
Sukhjit <sukhaji65@gmail.com>
2025-01-09 22:00:18 +0530

    Remove unused font imports and simplify RootLayout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "HealthFlow",
@@ -20,9 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body      >
         <AuthProvider>
         {children}
         </AuthProvider>
